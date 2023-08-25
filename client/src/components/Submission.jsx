35206952eed4388e00a135de0826cb0578f29944
Antpoo <1jiangand2@gmail.com>
2023-08-25 01:16:27 -0400

    Fixed submission box

--- client/src/components/Submission.jsx
+++ client/src/components/Submission.jsx
@@ -1,24 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 class Submissions extends Component {
+
+    
+
     constructor(props) {
         super(props);
         this.state = {
-            value: ""
+            value: "",
+            lastBrCount: 0
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+
     handleChange = (event) => {
-        this.setState({value: event.target.value});
+        this.setState({value: event.target.value}, () => {
+            this.adjustTextAreaHeight();
+        });
+    }
+
+    adjustTextAreaHeight = () => {
+        const textArea = this.textAreaRef;
+        textArea.style.height = 'auto';
+        textArea.style.height = `${textArea.scrollHeight}px`;
     }
 
     handleSubmit = (event) => {
         alert(this.state.value);
         event.preventDefault();
     }
+
     
 
     render() {
@@ -29,8 +43,14 @@ class Submissions extends Component {
                         Submit your program here:
                     </header>
 
-                    <textarea className="bg-slate-200 hover:bg-slate-300 mt-4 outline:none outline-none" value={this.state.value} onChange={this.handleChange} />
-                    <br/>
+                    <textarea 
+                        ref={(ref) => (this.textAreaRef = ref)}
+                        className="bg-slate-200 hover:bg-slate-300 mt-4 outline:none outline-none" 
+                        rows='20'
+                        cols='200'
+                        value={this.state.value} 
+                        onChange={this.handleChange} />
+                    <br />
                     <button className="py-2 px-4 bg-green-400 text-white text-xl hover:bg-green-500 rounded border-b-4 border-green-600 hover:border-green-800 mt-4">
                         Submit
                     </button>
