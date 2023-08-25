a7ad85d8870a5abb9a2fa181885bdab5eb210b74
Squishyguy10 <squishyguy10@Gmail.com>
2023-08-25 03:32:08 -0400

    changed game stuff

--- client/src/components/Games.jsx
+++ client/src/components/Games.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import { PuzzlePieceIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
-import { games } from './datajsons/gamedata';
 
+const games = [
+    {
+        title: 'Tic Tac Toe',
+        description: 'The tic tac toe you know and love! One player is X whilst the other is O. X goes first. Place your piece in any vacant square. First to get 3 in a row in any direction wins!',
+        page: '/tictactoe',
+        image: 'tictactoe.svg',
+    },
+    {
+        title: 'Nim',
+        description: 'There is some number of piles of stones and each pile has some number of stones. Player A goes first and player B goes second. On each turn, you may remove any number of stones from any single pile. Whoever removes the last stone from the last remaining pile wins.',
+        page: '/nim',
+        image: '',
+    },
+    {
+        title: 'Splix',
+        description: 'You start with a small square of territory. You may move in any cardinal direction, and once you venture out of your territory you claim other territory by returning back to your own area. The area you enclosed will become part of your area, just don\'t get hit while you are away from home!',
+        page: '/splix',
+        image: '',
+    },
+]
+
+    
 class Games extends Component {
-
 
     render() {
         return (
@@ -29,13 +49,12 @@ class Games extends Component {
                                     className='absolute inset-0 w-full h-full object-cover object-center py-10 '
                                     src={game.image}
                                 />
-                            
-                                <div className='px-8 py-10 relative z-10 w-full border-4 border-blue-500 bg-blue-900 opacity-0 hover:opacity-100'>
+                                <div className='px-8 py-10 relative z-10 w-full border-4 border-blue-500 bg-blue-900 opacity-0 hover:opacity-100 transition ease-in-out delay-100 duration-300'>
                                     <h1 className='font-bold title-font text-lg font-medium text-white mb-3'>
                                         {game.title}
                                     </h1>
                                     <p className='leading-relaxed text-white'>{game.description}</p>
-                                </div>
+                                </div> 
                             </div>
                         </Link>
                     )))}
