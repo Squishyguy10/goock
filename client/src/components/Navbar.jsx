a6f1ef12d4b8c41701a412228deac9f8078c3e6d
Antpoo <1jiangand2@gmail.com>
2023-08-24 22:43:17 -0400

    Fixed app and stuff

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,25 @@ import React from 'react';
 
 function Navbar() {
 
+    return (
+        <header className='bg-gray-800 md:sticky top-0 z-10'>
+            <div className='container flex flex-wrap p-2 flex-col md:flex-row items-center text-gray-300'>
+                <a className='title-font font-medium mb-4 md:mb-0'>
+                    <a href='/' className='ml-3 text-2xl text-gray-100'>
+                        Goock Games
+                    </a>
+                </a>
+                <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-xl justify-center'>
+                    <a href='/games' className='mr-5 hover:text-white'>
+                        Games
+                    </a>
+                    <a href='/leaderboards' className='mr-5 hover:text-white'>
+                        Leaderboards
+                    </a>
+                </nav>
+            </div>
+        </header>
+    );
 }
 
 export default Navbar;
