675450fb26b924b68bc092cbfdea837735ce9d55
Squishyguy10 <squishyguy10@Gmail.com>
2023-08-24 22:44:21 -0400

    made (not rly working) submission

--- client/src/components/Nim.jsx
+++ client/src/components/Nim.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Submission from "./Submission";
 
 class Nim extends Component {
     render () {
         return (
             <div>
-
+                <Submission />
             </div>
         );
     }
--- client/src/components/Submission.jsx
+++ client/src/components/Submission.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 
 class Submissions extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            code: "Pease submit your code here:"
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({code: event.target.code});
+    }
+
+    handleSubmit(event) {
+        alert(this.state.code);
+        event.preventDefault();
+    }
+
     render () {
         return (
-            <div>
-
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Submit your program here:
+                    <textarea value={this.state.code} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 }
