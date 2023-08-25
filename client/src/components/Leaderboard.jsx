9e952c38601687f68e1d12ae66a39c302381fe6c
qualya <102568306+qualya@users.noreply.github.com>
2023-08-25 04:38:42 -0400

    add leaderboard requesting to client

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
+const socket = new WebSocket('ws://localhost:3001');
+var leaderData;
+
 class Leaderboard extends Component {
 
     constructor(props) {
         this.state = {
             game: props.game,
         };
+		this.getLeaderboardData();
     }
+	
+	getLeaderboardData() {
+		let leaderboardRequest = {
+			type: "requestLeaderboard",
+			game: this.state.game
+		};
+		socket.send(JSON.stringify(leaderboardRequest));
+		
+		socket.addEventListener('message', (event) => {
+			leaderData = JSON.parse(event.data);
+		});
+	}
 
     render() {
         return (
