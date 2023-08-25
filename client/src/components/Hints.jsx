6b3cb44689bba4b8f9751e7c12ea00137175df9d
Squishyguy10 <squishyguy10@Gmail.com>
2023-08-25 17:24:15 -0400

    gradient bg for hints

--- client/src/components/Hints.jsx
+++ client/src/components/Hints.jsx
@@ -17,40 +17,39 @@ const games = [
 ]
 
 function Hints() {
-
     return (
-        <div className='container px-5 mx-auto text-center lg:px-40'>
-            <div className='flex flex-col w-full mb-10'>
-                <QuestionMarkCircleIcon className='mx-auto inline-block w-10 py-10' />
-                <h1 className='sm:text-4xl text-3xl font-medium title-font font-display mb-5'>
-                    Hints
-                </h1>
-                <p className='lg:w-2/3 mx-auto leading-relaxed text-base'>
-                    Spoiler alert! Hover over each game to see a hint for the strategy!
-                </p>
-            </div>
-            <div className='flex justify-center flex-wrap'>
-                {games.map((game => (    
-                    <div>     
-                        <h1 className='text-2xl'>
-                            {game.name}
-                        </h1>
+        <div className='bg-gradient-to-b from-amber-100 via-amber-100 to-white'>
+            <div className='container px-5 mx-auto text-center lg:px-40'>
+                <div className='flex flex-col w-full mb-10'>
+                    <QuestionMarkCircleIcon className='mx-auto inline-block w-10 py-10' />
+                    <h1 className='sm:text-4xl text-3xl font-medium title-font font-display mb-5'>
+                        Hints
+                    </h1>
+                    <p className='lg:w-2/3 mx-auto leading-relaxed text-base'>
+                        Spoiler alert! Hover below each game to see a hint for the strategy!
+                    </p>
+                </div>
+                <div className='flex justify-center flex-wrap'>
+                    {games.map((game => (    
+                        <div>     
+                            <h1 className='text-2xl'>
+                                {game.name}
+                            </h1>
 
-                        <ChevronDoubleDownIcon className='mx-auto inline-block w-10 pt-10' />
+                            <ChevronDoubleDownIcon className='mx-auto inline-block w-10 pt-10' />
 
-                        <div className='flex relative py-5'>
-                            
-                            <div className='px-8 py-10 relative z-10 w-full border-4 border-amber-500 bg-amber-900 opacity-0 hover:opacity-100 transition-x-20 ease-in-out duration-500'>
-                                <p className='leading-relaxed text-white'>{game.hint}</p>
-                            </div> 
+                            <div className='flex relative py-5'>
+                                <div className='px-8 py-10 relative z-10 w-full border-4 border-amber-500 bg-amber-900 opacity-0 hover:opacity-100 transition-x-20 ease-in-out duration-500'>
+                                    <p className='leading-relaxed text-white'>{game.hint}</p>
+                                </div> 
+                            </div>
                         </div>
-                    </div>
-                )))}
+                    )))}
+                </div>
+                <br /><br /><br />
             </div>
-            
         </div>
     );
-
 }
 
 export default Hints;
