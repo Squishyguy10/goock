6d5fa0bfa3443f1a405667f9a1ff9e4aef520eff
Squishyguy10 <squishyguy10@Gmail.com>
2023-08-25 17:13:12 -0400

    i9 cry

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,30 +4,6 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { Link } from 'react-router-dom';
 
 
-const games = [
-    {
-        title: 'Tic Tac Toe',
-        description: 'The tic tac toe you know and love! One player is X whilst the other is O. X goes first. Place your piece in any vacant square. First to get 3 in a row in any direction wins!',
-        page: '/tictactoe',
-        image: 'tictactoe.svg',
-        num: 0,
-    },
-    {
-        title: 'Nim',
-        description: 'There is some number of piles of stones and each pile has some number of stones. Player A goes first and player B goes second. On each turn, you may remove any number of stones from any single pile. Whoever removes the last stone from the last remaining pile wins.',
-        page: '/nim',
-        image: 'nim.svg',
-        num: 1,
-    },
-    {
-        title: 'Pong',
-        description: 'The notorius arcade game! One ball moves across the screen, and 2 players—with their paddle on the side of the screen—move their paddles vertically to deflect the ball. If the ball goes past your paddle, you lose.',
-        page: '/pong',
-        image: 'pong.webp',
-        num: 2,
-    },
-]
-
 function Carousel({slides}) {
     let [current, setCurrent] = useState(0);
     
@@ -39,23 +15,30 @@ function Carousel({slides}) {
         current === slides.length-1 ? setCurrent(0) : setCurrent(current+1);
     };
 
-
     return (
         <div className="overflow-hidden relative">
-            
-
             <div className={`flex transition ease-out duration-400`} style={{ transform: `translateX(-${current*100}%)` }}>
                 {slides.map((s) => { 
-                    return <img src={s} alt={"games"} className=""/>; 
+                    return (
+                        <> 
+                            <Link to={s.page}>
+                                <button className='absolute ml-[43%] mt-10 top-0 justify-center py-2 px-4 bg-gray-200 text-gray-800 text-xl hover:bg-gray-400 rounded border-b-4 border-gray-400 hover:border-gray-600'>
+                                    {s.title}
+                                </button>
+                            </Link>
+                            <img src={s.image} alt={"games"} className=""/>  
+                        </>
+                    );
                 })}
             </div>
+            
+            
 
-
-            <div className="absolute top-0 h-full w-full justify-between flex">
-                <button onClick={prevSlide} aria-label="left" size="small" className="text-purple-700">
+            <div className="absolute top-0 w-full justify-between flex">
+                <button onClick={prevSlide} className="text-red-500">
                     <ArrowBackIosNewIcon />
                 </button>
-                <button onClick={nextSlide} aria-label="right" size="small" className="text-purple-700">
+                <button onClick={nextSlide} className="text-red-500">
                     <ArrowForwardIosIcon />
                 </button>
             </div>
--- client/src/components/Games.jsx
+++ client/src/components/Games.jsx
@@ -5,10 +5,25 @@ import { PuzzlePieceIcon } from '@heroicons/react/24/outline';
 class Games extends Component {
     render() {
         const slides = [
-            'tictactoe.svg',
-            'nim.svg',
-            'pong.webp'
-        ];
+            {
+                title: 'Tic Tac Toe',
+                description: 'The tic tac toe you know and love! One player is X whilst the other is O. X goes first. Place your piece in any vacant square. First to get 3 in a row in any direction wins!',
+                page: '/tictactoe',
+                image: 'tictactoe.svg',
+            },
+            {
+                title: 'Nim',
+                description: 'There is some number of piles of stones and each pile has some number of stones. Player A goes first and player B goes second. On each turn, you may remove any number of stones from any single pile. Whoever removes the last stone from the last remaining pile wins.',
+                page: '/nim',
+                image: 'nim.svg',
+            },
+            {
+                title: 'Pong',
+                description: 'The notorius arcade game! One ball moves across the screen, and 2 players—with their paddle on the side of the screen—move their paddles vertically to deflect the ball. If the ball goes past your paddle, you lose.',
+                page: '/pong',
+                image: 'pong.webp',
+            },
+        ]
 
         return (
             <div className="bg-amber-100">
