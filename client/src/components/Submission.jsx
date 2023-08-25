af5ce48cdf3c634addf1e70ecf3575d9f4365a20
Squishyguy10 <squishyguy10@Gmail.com>
2023-08-25 00:49:48 -0400

    i cry

--- client/src/components/Submission.jsx
+++ client/src/components/Submission.jsx
@@ -19,6 +19,7 @@ class Submissions extends Component {
         alert(this.state.value);
         event.preventDefault();
     }
+    
 
     render() {
         return (
@@ -28,7 +29,7 @@ class Submissions extends Component {
                         Submit your program here:
                     </header>
 
-                    <textarea rows="50" cols="230" className="bg-slate-300 hover:bg-slate-400 mt-4 outline:none" value={this.state.value} onChange={this.handleChange} />
+                    <textarea className="bg-slate-200 hover:bg-slate-300 mt-4 outline:none outline-none" value={this.state.value} onChange={this.handleChange} />
                     <br/>
                     <button className="py-2 px-4 bg-green-400 text-white text-xl hover:bg-green-500 rounded border-b-4 border-green-600 hover:border-green-800 mt-4">
                         Submit
