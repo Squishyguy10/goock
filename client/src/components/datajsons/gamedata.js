6917a943d48ade121e06acbd144667cc9911ed3c
Antpoo <1jiangand2@gmail.com>
2023-08-25 02:37:27 -0400

    Started games thing

---
+++ client/src/components/datajsons/gamedata.js
@@ -0,0 +1,20 @@
+export const games = [
+    {
+        title: 'Tic Tac Toe',
+        description: 'The tic tac toe you know and love! One player is X whilst the other is O. X goes first. Place your piece in any vacant square. First to get 3 in a row in any direction wins!',
+        page: '/tictactoe',
+        image: 'tic-tac-toe.png',
+    },
+    {
+        title: 'Nim',
+        description: 'There is some number of piles of stones and each pile has some number of stones. Player A goes first and player B goes second. On each turn, you may remove any number of stones from any single pile. Whoever removes the last stone from the last remaining pile wins.',
+        page: '/nim',
+        image: '',
+    },
+    {
+        title: 'Splix',
+        description: 'You start with a small square of territory. You may move in any cardinal direction, and once you venture out of your territory you claim other territory by returning back to your own area. The area you enclosed will become part of your area, just don\'t get hit while you are away from home!',
+        page: '/splix',
+        image: '',
+    },
+]
