9256c3a9696782483cdc4e19581a165d6abe54d0
Squishyguy10 <squishyguy10@Gmail.com>
2023-08-24 23:34:02 -0400

    testy

--- client/src/components/Games.jsx
+++ client/src/components/Games.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Submission from "./Submission"
 
 class Games extends Component {
     render () {
         return (
             <div>
-
+                <Submission />
             </div>
         );
     }
--- client/src/components/Submission.jsx
+++ client/src/components/Submission.jsx
@@ -4,7 +4,7 @@ class Submissions extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            code: "Pease submit your code here:"
+            code: ""
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -23,11 +23,16 @@ class Submissions extends Component {
     render () {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>
-                    Submit your program here:
-                    <textarea value={this.state.code} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+                <div className="ml-3 mt-5">
+                    <header className="text-xl">
+                        Submit your program here:
+                    </header>
+
+                    <textarea className="bg-slate-300 hover:bg-slate-400 mt-4" value={this.state.code} onChange={this.handleChange} />
+                    <br></br>
+                    <input type="submit" value="Submit" className="py-2 px-4 bg-blue-400 text-white text-xl hover:bg-blue-500 rounded border-b-4 border-blue-600 hover:border-blue-800 mt-4"/>
+
+                </div>
             </form>
         );
     }
