f62091f978fcc3fcc2c8696a9fc08fdad230800c
qualya <102568306+qualya@users.noreply.github.com>
2023-08-25 02:08:23 -0400

    start nodeJS server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+var http = require('http');
+var server = http.createServer(app);
+var { Server } = require("socket.io");
+var io = new Server(server);
+const fs = require('fs');
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
@@ -15,3 +20,17 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+
+io.on('connection', (socket) => {
+	socket.on('submitCode', codeSubmission => {
+		fs.writeFile("/pending/"+codeSubmission.title+".py", codeSubmission.code, (err) => {
+			if (err) {
+				console.log(err);
+			}
+			else {
+				console.log(codeSubmission.title + " was submitted.");
+			}
+		});
+	}
+}
