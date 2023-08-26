1472bbf401759ea886529c6fd5d8ac4603243b88
Squishyguy10 <squishyguy10@Gmail.com>
2023-08-25 20:58:11 -0400

    created postsub (krik)

---
+++ client/src/components/PostSubmission.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Submission extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Submitted! Please wait for the server to run it...
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default Submission;
