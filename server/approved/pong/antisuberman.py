1dd876aeb2a4b1816a70c426b6b06a0ccf66b1a4
qualya <102568306+qualya@users.noreply.github.com>
2023-08-26 04:02:40 -0400

    fix antisuberman

--- server/approved/pong/antisuberman.py
+++ server/approved/pong/antisuberman.py
@@ -8,9 +8,9 @@ while zoo:
     turn, x, y, dx, dy, py, py2 = map(int, input().split(","))
     
     if turn == 0:
-        centre = (py+pheight)/2
+        centre = py+(pheight/2)
     if turn == 1:
-        centre = (py2+pheight)/2
+        centre = py2+(pheight/2)
         
     if y > centre:
         print("down")
